f45cfc58f52339318bf701a818c9b2d43df81754
leo Sayous <leo@sismo.io>
2022-12-19 18:38:07 +0100

    fix: groups that was not generated correctly

--- group-generators/helpers/data-providers/snapshot/provider.ts
+++ group-generators/helpers/data-providers/snapshot/provider.ts
@@ -39,7 +39,7 @@ export default class SnapshotProvider
     input: QuerySpaceVotersInput,
     defaultValue = 1
   ): Promise<FetchedData> {
-    const chunkSize = 20000;
+    const chunkSize = 1000;
     let currentChunkIndex = 0;
     let currentChunkVoters: { voter: string }[] = [];
     const fetchedData: { [address: string]: number } = {};
